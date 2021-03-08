2f106f53008a21f0c290ba4fc6739f035b3e17aa
Tahid <mdtahidurrahman20@gmail.com>
2021-03-08 18:01:55 +0600

    api

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React from "react";
-import {  StyleSheet, TextInput, TouchableHighlight, View } from "react-native";
+import {  DrawerLayoutAndroid, StyleSheet, TextInput, TouchableHighlight, View } from "react-native";
 import ActivityIndicatorPart from "./ActivityIndicator";
 import Buttons from "./Buttons";
+import DrawerLayoutAndroidPart from "./DrawerLayoutAndroidPart";
 import FlatListBasics from "./FlatListBasics";
 import Images from "./Images";
 import KeyboardAvoidingViews from "./KeyboardAvoidingViews";
@@ -19,25 +20,30 @@ import TouchableHighlightExample from "./TouchableHightLights";
 export default function App() {
   return (
     <View style={styles.container}>
-      {/* <PizzaTranslator /> */}
-
-    {/* BuiltInComponents */}
-
-    {/* <ActivityIndicatorPart /> */}
-    {/* <Buttons /> */}
-    {/* <FlatListBasics /> */}
-    {/* <Images/> */}
-    {/* <KeyboardAvoidingViews /> */}
-    {/* <Modals/> */}
-    {/* <PullToRefresh /> */}
-    {/* <ScrollViewPart /> */}
-
-      {/* <SectionListBasics /> */}
-      {/* <StatusBarPart /> */}
-      {/* <Switches/> */}
-      {/* <Texts/> */}
-      {/* <TextInputs/> */}
+      
+      {/* 🍁 Components  🌹*/}
+      {/* <View>
+      <PizzaTranslator />
+      <ActivityIndicatorPart />
+      <Buttons />
+      <FlatListBasics />
+      <Images/>
+      <KeyboardAvoidingViews />
+      <Modals/>
+      <PullToRefresh />
+      <ScrollViewPart />
+      <SectionListBasics />
+      <StatusBarPart />
+      <Switches/>
+      <Texts/>
+      <TextInputs/>
       <TouchableHighlightExample />
+      <DrawerLayoutAndroidPart />
+      </View> */}
+
+      {/* 🎇 API 🎆🎇 */}
+
+
     </View>
   );
 }
